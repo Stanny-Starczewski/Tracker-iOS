c4f315b649d6646ce5dfa7725d90445b1af7f6d6
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-05-11 14:49:47 -0400

    Initial Commit

---
+++ Tracker-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker-iOS
+//
+//  Created by Gennadii Kulikov on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
