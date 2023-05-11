37aa2239c4606275687140a4484ad880ebe7aef7
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-05-11 18:25:07 -0400

    - Filled in TabBarViewController & done - Added TrackersViewController & StatisticViewController

--- Tracker-iOS/TabBarViewController.swift
+++ Tracker-iOS/TabBarViewController.swift
@@ -4,9 +4,31 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tabBar.tintColor = .Blue
+        tabBar.barTintColor = .Gray
+        tabBar.backgroundColor = .WhiteDay
+
+        tabBar.layer.borderColor = UIColor.LightGray.cgColor
+        tabBar.layer.borderWidth = 1
+        tabBar.layer.masksToBounds = true
+        
+        let trackersViewController = TrackersViewController()
+        let statisticViewController = StatisticViewController()
+        
+        trackersViewController.tabBarItem = UITabBarItem(
+            title: "Трекеры",
+            image: UIImage(named: "Trackers_TabBarIcon_on"),
+            selectedImage: nil
+        )
+        statisticViewController.tabBarItem = UITabBarItem(
+            title: "Статистика",
+            image: UIImage(named: "Statistics_TabBarIcon_off"),
+            selectedImage: nil
+        )
+        
+        let controllers = [trackersViewController, statisticViewController]
+        
+        viewControllers = controllers
     }
-
-
 }
-
