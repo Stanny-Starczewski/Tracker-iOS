9ed02ffa0a193613875035f97673b384afc7bb74
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-05-11 20:57:18 -0400

    Added ViewControllers group (for TabBarViewController, TrackersViewController & StatisticViewController) Added Helpers group (for Colors & added Emoji, added ShowPreviewUI) Added Button + on TrackersViewController

--- Tracker-iOS/ViewControllers/TrackersViewController.swift
+++ Tracker-iOS/ViewControllers/TrackersViewController.swift
@@ -1,9 +1,61 @@
 import UIKit
 
- final class TrackersViewController: UIViewController {
+final class TrackersViewController: UIViewController {
+    
+    private lazy var addButton: UIButton = {
+        let button = UIButton.systemButton(
+            with: UIImage(
+                systemName: "plus",
+                withConfiguration: UIImage.SymbolConfiguration(
+                    pointSize: 18,
+                    weight: .bold
+                )
+            )!,
+            target: self,
+            action: #selector(didTapPlusButton))
+        button.tintColor = .BlackDay
+        return button
+    }()
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .WhiteDay
+        configureViews()
+        configureConstraints()
+        
+    }
+    
+    // MARK: - Actions
+    
+    @objc
+    private func didTapPlusButton() {
+        print("Plus tapped")
+    }
+}
+ //MARK: - Layout methods
 
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         view.backgroundColor = .WhiteDay
-     }
- }
+private extension TrackersViewController {
+    func configureViews() {
+        view.backgroundColor = .WhiteDay
+        view.addSubview(addButton)
+
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    func configureConstraints() {
+        NSLayoutConstraint.activate([
+            addButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 18),
+            addButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 18),
+        ])
+    }
+}
+
+// MARK: - SHOW PREVIEW
+
+import SwiftUI
+struct CreateTrackerVCProvider: PreviewProvider {
+    static var previews: some View {
+        TrackersViewController().showPreview()
+    }
+}
