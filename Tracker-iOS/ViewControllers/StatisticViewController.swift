b616ac6f82468561b84f63e15de371ab0c138e31
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-05-11 23:11:56 -0400

    EmpryStat icon & title "Статистика"" added to StatisticViewController

--- Tracker-iOS/ViewControllers/StatisticViewController.swift
+++ Tracker-iOS/ViewControllers/StatisticViewController.swift
@@ -1,12 +1,71 @@
 import UIKit
 
- final class StatisticViewController: UIViewController {
+final class StatisticViewController: UIViewController {
+    
+    // MARK: - Layout elements
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Статистика"
+        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        return label
+    }()
+    
+    private let emptyImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "EmptyStat")
+        return imageView
+    }()
+    
+    private let emptyLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.text = "Анализировать пока нечего"
+        label.textColor = .BlackDay
+        return label
+    }()
+    
+    private let emptyStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.spacing = 8
+        return stack
+    }()
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupContent()
+        setupConstraints()
+    }
+    
+}
 
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         view.backgroundColor = .WhiteDay
-     }
- }
+// MARK: - Layout methods
+private extension StatisticViewController {
+    func setupContent() {
+        view.backgroundColor = .WhiteDay
+        view.addSubview(nameLabel)
+        view.addSubview(emptyStack)
+        
+        emptyStack.addArrangedSubview(emptyImageView)
+        emptyStack.addArrangedSubview(emptyLabel)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 52),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            emptyStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptyStack.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
 // MARK: - SHOW PREVIEW
 
 import SwiftUI
