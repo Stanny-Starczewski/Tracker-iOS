c88a09db945bdf0755e29aa3111de7935e9f9cc1
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-05-11 21:29:44 -0400

    Label with "Трекеры" name added to TrackersViewController

--- Tracker-iOS/ViewControllers/TrackersViewController.swift
+++ Tracker-iOS/ViewControllers/TrackersViewController.swift
@@ -2,6 +2,13 @@ import UIKit
 
 final class TrackersViewController: UIViewController {
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Трекеры"
+        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        return label
+    }()
+    
     private lazy var addButton: UIButton = {
         let button = UIButton.systemButton(
             with: UIImage(
@@ -38,15 +45,19 @@ final class TrackersViewController: UIViewController {
 private extension TrackersViewController {
     func configureViews() {
         view.backgroundColor = .WhiteDay
+        view.addSubview(titleLabel)
         view.addSubview(addButton)
 
         addButton.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
     }
 
     func configureConstraints() {
         NSLayoutConstraint.activate([
-            addButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 18),
-            addButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 18),
+            addButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 13),
+            addButton.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            titleLabel.topAnchor.constraint(equalTo: addButton.bottomAnchor, constant: 13)
         ])
     }
 }
