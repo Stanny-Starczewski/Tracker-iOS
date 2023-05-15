9bcdcdfe4e91aa98bc62f6b771986540482c676d
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-05-15 13:36:01 -0400

    Additional changes to make it working

--- Tracker-iOS/Extensions/Colors+Extension.swift
+++ Tracker-iOS/Extensions/Colors+Extension.swift
@@ -15,5 +15,26 @@ extension UIColor {
     static var BackgroundDay: UIColor { UIColor(named: "BackgroundDay") ?? UIColor.gray }
     
     // MARK: - ActionsCells (habits) colors
-    static func bunchOfSChoices(_ number: Int) -> UIColor? { UIColor(named: "ColorSelection\(number)") }
+//    static func bunchOfSChoices(_ number: Int) -> UIColor? { UIColor(named: "ColorSelection\(number)") }
+    
+    static let bunchOfSChoices = [
+        UIColor(named: "ColorSelection1")!,
+        UIColor(named: "ColorSelection2")!,
+        UIColor(named: "ColorSelection3")!,
+        UIColor(named: "ColorSelection4")!,
+        UIColor(named: "ColorSelection5")!,
+        UIColor(named: "ColorSelection6")!,
+        UIColor(named: "ColorSelection7")!,
+        UIColor(named: "ColorSelection8")!,
+        UIColor(named: "ColorSelection9")!,
+        UIColor(named: "ColorSelection10")!,
+        UIColor(named: "ColorSelection11")!,
+        UIColor(named: "ColorSelection12")!,
+        UIColor(named: "ColorSelection13")!,
+        UIColor(named: "ColorSelection14")!,
+        UIColor(named: "ColorSelection15")!,
+        UIColor(named: "ColorSelection16")!,
+        UIColor(named: "ColorSelection17")!,
+        UIColor(named: "ColorSelection18")!
+    ]
 }
--- Tracker-iOS/UIStructs/Tracker.swift
+++ Tracker-iOS/UIStructs/Tracker.swift
@@ -14,4 +14,35 @@ struct Tracker: Identifiable {
          self.color = color
          self.schedule = schedule
      }
- }
+    
+    init(tracker: Tracker) {
+        self.id = tracker.id
+        self.label = tracker.label
+        self.emoji = tracker.emoji
+        self.color = tracker.color
+        self.schedule = tracker.schedule
+    }
+    
+    init(data: Data) {
+        guard let emoji = data.emoji, let color = data.color else { fatalError() }
+        
+        self.id = UUID()
+        self.label = data.label
+        self.emoji = emoji
+        self.color = color
+        self.schedule = data.schedule
+    }
+    
+    var data: Data {
+        Data(label: label, emoji: emoji, color: color, schedule: schedule)
+    }
+}
+
+extension Tracker {
+    struct Data {
+        var label: String = ""
+        var emoji: String? = nil
+        var color: UIColor? = nil
+        var schedule: [WeekDay]? = nil
+    }
+}
